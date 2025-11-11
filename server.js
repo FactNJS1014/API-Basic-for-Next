b24430a04ea18989f5179aa6e0be2c11a58c9136
Natdanai <natda002@gmail.com>
2025-11-11 19:24:30 +0700

    edit port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 8080;
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const SECRET_KEY = 'mysecretkey';
